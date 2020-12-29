4044c30d33896a59ba4840a8558ff82b563f29e3
Speykious <speykious@gmail.com>
2020-12-30 00:46:05 +0100

    testn't

--- test/AutomaTest.hs
+++ test/AutomaTest.hs
@@ -1,7 +1,10 @@
 module Main (main) where
 
+import Prelude hiding (null)
 import Automaton
-import Data.List
+import DotShow
+import Data.List (foldl', intercalate)
+import Data.Set (Set, null, fromList)
 
 s0 = S 0 (False, False)
 s1 = S 1 (False, True)
@@ -14,9 +17,32 @@ t2 = T s1 'a' s0
 t3 = T s1 'b' s2
 t4 = T s2 'a' s0
 
+ts = [t0, t1, t2, t3, t4]
+automaton = fsm "A" ts
+
+showSetg :: (Show a) => String -> Set a -> String
+showSetg sep s
+  | null s    = "{}"
+  | otherwise = "{ " ++ intercalate sep (foldl' (\acc a -> show a : acc) [] s) ++ " }"
+
+showSet :: (Show a) => Set a -> String
+showSet = showSetg ", "
+
+showSetLn :: (Show a) => Set a -> String
+showSetLn = showSetg "\n, "
+
+printSet :: (Show a) => Set a -> IO ()
+printSet = putStrLn . showSet
+printSetLn :: (Show a) => Set a -> IO ()
+printSetLn = putStrLn . showSetLn
+
 main :: IO ()
 main = do
   putStr $ bld ++ "States:" ++ clr ++ " "
   print [s0, s1, s2, s3]
   putStrLn $ bld ++ "Transitions:" ++ clr
-  putStrLn $ unlines $ ("  " ++) . show <$> [t0, t1, t2, t3, t4]
+  putStrLn $ unlines $ ("  " ++) . show <$> ts
+  putStrLn ""
+  printSetLn $ fromList (t0:ts)
+  putStrLn ""
+  printSet $ states automaton
