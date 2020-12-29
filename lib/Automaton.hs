98bba3e5999065add3c797fb252e4a294bb17ee9
Speykious <speykious@gmail.com>
2020-12-29 23:47:29 +0100

    FSM, changing Show instance, and implementing DotShow instances

--- lib/Automaton.hs
+++ lib/Automaton.hs
@@ -1,19 +1,21 @@
 module Automaton where
 
-import Data.Set
+import DotShow
+import Data.Set (Set)
 
-data S a = S a (Bool, Bool)   deriving (Eq, Ord) -- State
-data T a = T (S a) Char (S a) deriving (Eq, Ord) -- Transition
---data FSM a =
+data S a = S a (Bool, Bool)      deriving (Eq, Ord) -- State
+data T a = T (S a) Char (S a)    deriving (Eq, Ord) -- Transition
+data FSM a b = FSM b (Set (T a)) deriving (Eq, Ord) -- Finite State Machine
 
 clr = "\x1b[0m"
 bld = "\x1b[1m"
+dim = "\x1b[2m"
 red = "\x1b[31m"
 grn = "\x1b[32m"
 yel = "\x1b[33m"
 
 instance Show a => Show (S a) where
-  show (S a (i, f)) = show a
+  show (S a (i, f)) = dim ++ "S" ++ clr ++ show a
                     ++ (if i then grn ++ "#I" ++ clr else "")
                     ++ (if f then red ++ "#F" ++ clr else "")
 
@@ -21,3 +23,17 @@ instance Show a => Show (T a) where
   show (T sa c sb) = show sa ++ " "
                    ++ yel ++ ">-" ++ [c] ++ "->" ++ clr
                    ++ " " ++ show sb
+
+instance Show a => DotShow (S a) where
+  dotShow (S a _) = "S" ++ show a
+  dotDeclare (S a (i, f)) = Just $ "S" ++ show a
+                          ++ case params of
+                            [a, b] -> " [" ++ a ++ "," ++ b ++ "];"
+                            [a]    -> " [" ++ a ++ "];"
+                            _      -> ";"
+    where params = filter (/= "") [ if i then "color=red" else ""
+                                  , if f then "peripheries=2" else "" ]
+
+instance Show a => DotShow (T a) where
+  dotShow (T sa c sb) = dotShow sa ++ " -> " ++ dotShow sb
+                      ++ " [label=" ++ [c] ++ "];"
