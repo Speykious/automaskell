1ffa2f8592fe6068f8025939fd4bba84d49f8c65
Speykious <speykious@gmail.com>
2020-12-29 22:41:41 +0100

    Basic test: showing states and transitions

--- test/AutomaTest.hs
+++ test/AutomaTest.hs
@@ -1,6 +1,22 @@
-module AutomaTest (main) where
+module Main (main) where
 
 import Automaton
+import Data.List
+
+s0 = S "s0" (False, False)
+s1 = S "s1" (False, True)
+s2 = S "s2" (True, False)
+s3 = S "s3" (True, True)
+
+t0 = T s0 'a' s1
+t1 = T s0 'b' s1
+t2 = T s1 'a' s0
+t3 = T s1 'b' s2
+t4 = T s2 'a' s0
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  putStr $ bld ++ "States:" ++ clr ++ " "
+  print [s0, s1, s2, s3]
+  putStrLn $ bld ++ "Transitions:" ++ clr
+  putStrLn $ unlines $ ("  " ++) . show <$> [t0, t1, t2, t3, t4]
