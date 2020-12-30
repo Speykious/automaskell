814aadc8f7213dbf8cc9ba2080ca09042a9ebc93
Speykious <speykious@gmail.com>
2020-12-30 03:42:56 +0100

    Adding a mapSet function

--- lib/Helpers.hs
+++ lib/Helpers.hs
@@ -1,7 +1,7 @@
 module Helpers where
 
 import Prelude hiding (null)
-import Data.Set (Set, null)
+import Data.Set (Set, null, empty, singleton)
 import Data.List (foldl', intercalate)
 
 showSetg :: (Show a) => String -> Set a -> String
@@ -19,3 +19,6 @@ printSet :: (Show a) => Set a -> IO ()
 printSet = putStrLn . showSet
 printSetLn :: (Show a) => Set a -> IO ()
 printSetLn = putStrLn . showSetLn
+
+mapSet :: (Foldable t1, Foldable t2, Monoid (t2 b)) => (a -> t2 b) -> t1 a -> t2 b
+mapSet f = foldl' (\acc a -> acc <> f a) mempty
