d9606b6ada3f14c37d356a7d605eaa6d580ad14e
Speykious <speykious@gmail.com>
2020-12-30 03:44:17 +0100

    PROGRESS on basics

--- lib/Automaton.hs
+++ lib/Automaton.hs
@@ -1,12 +1,13 @@
 module Automaton where
 
 import DotShow
+import Helpers
 import Data.Set (Set, fromList, empty)
-import Data.List (foldl')
+import Data.List (foldl', lines, intercalate)
 
 data S a = S a (Bool, Bool)      deriving (Eq, Ord) -- State
 data T a = T (S a) Char (S a)    deriving (Eq, Ord) -- Transition
-data FSM a b = FSM b (Set (T a)) deriving (Eq, Ord) -- Finite State Machine
+data FSM a = FSM String (Set (T a)) deriving (Eq, Ord) -- Finite State Machine
 
 clr = "\x1b[0m"
 bld = "\x1b[1m"
@@ -25,8 +26,15 @@ instance Show a => Show (T a) where
                    ++ yel ++ ">-" ++ [c] ++ "->" ++ clr
                    ++ " " ++ show sb
 
-instance (Show a, Show b) => Show (FSM a b) where
-  show (FSM l st) = undefined
+instance (Ord a, Show a) => Show (FSM a) where
+  show (FSM l st) = bld ++ "Automaton " ++ show l ++ clr ++ ":\n"
+                  ++ bld ++ "  States" ++ clr ++ ":\n    "
+                  ++ indentedSet (statesFromTransitions st) ++ "\n"
+                  ++ bld ++ "  Transitions" ++ clr ++ ":\n    "
+                  ++ indentedSet st
+    where indentedSet :: (Show a) => Set a -> String
+          indentedSet = intercalate "\n    " . lines . showSetLn
+
 
 instance Show a => DotShow (S a) where
   dotShow    (S a _)      = "S" ++ show a
@@ -42,8 +50,18 @@ instance Show a => DotShow (T a) where
   dotShow (T sa c sb) = dotShow sa ++ " -> " ++ dotShow sb
                       ++ " [label=" ++ [c] ++ "];"
 
-states :: Ord a => FSM a b -> Set (S a)
-states (FSM l st) = foldl' (\acc (T sa _ sb) -> acc <> fromList [sa, sb]) empty st
+statesFromTransitions :: Ord a => Set (T a) -> Set (S a)
+statesFromTransitions = mapSet $ \(T sa _ sb) -> fromList [sa, sb]
+                        --foldl' (\acc (T sa _ sb) -> acc <> fromList [sa, sb]) empty
 
-fsm :: (Ord a) => b -> [T a] -> FSM a b
-fsm b ts = FSM b $ fromList ts
+states :: Ord a => FSM a -> Set (S a)
+states (FSM l st) = statesFromTransitions st
+
+fsm :: (Ord a) => String -> [T a] -> FSM a
+fsm l ts = FSM l $ fromList ts
+
+transitionsFromState :: FSM a -> S a -> Set (T a)
+transitionsFromState = undefined
+
+succStates :: FSM a -> S a -> Char -> Set (S a)
+succStates (FSM l st) s c = undefined
