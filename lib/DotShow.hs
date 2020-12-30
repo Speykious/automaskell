b56ab847c9145b4d2f940927485f9007366440e2
Speykious <speykious@gmail.com>
2020-12-30 19:36:11 +0100

    dotShowPDF done!

--- lib/DotShow.hs
+++ lib/DotShow.hs
@@ -1,6 +1,16 @@
 module DotShow where
 
+import System.Process (callCommand)
+
 class DotShow a where
   dotShow :: a -> String
   dotDeclare :: a -> Maybe String
   dotDeclare = const Nothing
+  
+  dotShowPDF :: String -> a -> IO ()
+  dotShowPDF name a = do
+      writeFile dotName (dotShow a)
+      callCommand $ "dot -Tpdf " ++ dotName ++ " -o " ++ pdfName
+      callCommand $ "xdg-open " ++ pdfName
+    where dotName = name ++ ".dot"
+          pdfName = name ++ ".pdf"
