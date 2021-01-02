b04704c5ca33da9b4aa64d4fd64363057da33f94
Speykious <speykious@gmail.com>
2021-01-02 02:20:42 +0100

    Examples

--- lib/TestConstants.hs
+++ lib/TestConstants.hs
@@ -18,50 +18,42 @@ s1 = S 1 (False, True)
 s2 = S 2 (True, False)
 s3 = S 3 (True, True)
 
-t0 = T s0 'a' s1
-td = T s0 'a' s2
-t1 = T s0 'b' s1
-t2 = T s1 'a' s0
-t3 = T s1 'b' s2
-t4 = T s2 'a' s0
-t5 = T s2 'b' s1
+ta = T s2 'b' s1
+tb = T s0 'a' s2
+ts = [ T s0 'b' s1
+     , T s0 'a' s1
+     , T s1 'a' s0
+     , T s1 'b' s2
+     , T s2 'a' s0 ]
 
 type A = Integer
 
-auto1 :: FSM A
-auto1 = fsmFromList "auto1" [t0, t1, t2, t3, t4, t5]
-auto1C :: FSM A
-auto1C = complete (-1) "ab" auto1
+auto :: FSM A
+auto = fsmFromList "auto" (ta:ts)
 auto2 :: FSM A
-auto2 = fsmFromList "auto2" [t0, td, t1, t2, t3, t4]
+auto2 = fsmFromList "auto2" (tb:ts)
 deter :: FSM (Set A)
 deter = convertToDFA auto2
-
 autodeter :: FSM (A, Set A)
-autodeter = auto1 <&> deter
+autodeter = auto <&> deter
 
+e1 = S 1 (True, False)
+e2 = S 2 (False, False)
+e3 = S 3 (False, True)
 
-nis :: S (A, Set A)
-nis = pairStates True (&&) (f auto1, f deter)
-  where f :: Ord a => FSM a -> S a
-        f = fromJust . fsmInitialState
+example1 :: FSM A
+example1 = fsmFromList "example1" [ T e1 'b' e1
+                                  , T e1 'a' e2
+                                  , T e2 'a' e2
+                                  , T e2 'b' e3
+                                  , T e3 'a' e3
+                                  , T e3 'b' e3 ]
+example2 :: FSM A
+example2 = fsmFromList "example2" [ T e1 'b' e2
+                                  , T e2 'b' e2
+                                  , T e2 'a' e3
+                                  , T e3 'b' e2
+                                  , T e3 'a' e3 ]
 
-stack :: Set (S (A, Set A))
-stack = DS.singleton nis
-
-sta :: Set (T A)
-stb :: Set (T (Set A))
-(sta, stb) = fsmTransFromPairState nis (auto1, deter)
-
-alpha :: String
-alpha = DS.elems $ DS.fromList (alphaFromTrans sta ++ alphaFromTrans stb)
-
-stp :: Set (T A, T (Set A))
-stp = fromList $ catMaybes $ (\c -> zipMaybe (cfind c sta, cfind c stb)) <$> alpha
-  where cfind c = find $ (== c) . symbol
-
-snt :: Set (T (A, Set A))
-snt = (\tp -> T nis (aFromTransPair tp) (pairEndingStates (&&) tp)) <<$>> stp
-
-sans :: Set (S (A, Set A))
-sans = DS.filter (not . (`labelMember` stack)) (endingStates snt)
+intexample = example1 <&> example2
+uniexample = example1 <|> example2
